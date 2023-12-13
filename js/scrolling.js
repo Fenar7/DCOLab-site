58cd25647ad73bf501b4ff68932de1c21d1eba7f
Fenar Khan <fenarkhan@gmail.com>
2023-12-13 16:08:46 +0530

    jksdjfslkfj

--- js/scrolling.js
+++ js/scrolling.js
@@ -1,30 +1,30 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const contentItems = document.querySelectorAll(".content-item");
-    let index = 0;
-    flag = 0;
+// document.addEventListener("DOMContentLoaded", function () {
+//     const contentItems = document.querySelectorAll(".content-item");
+//     let index = 0;
+//     flag = 0;
   
-    setInterval(() => {
+//     setInterval(() => {
 
-      const currentContent = contentItems[index];
-      const nextIndex = (index + 1) % contentItems.length;
-      const nextContent = contentItems[nextIndex];
+//       const currentContent = contentItems[index];
+//       const nextIndex = (index + 1) % contentItems.length;
+//       const nextContent = contentItems[nextIndex];
 
   
-      currentContent.classList.remove("active");
-      currentContent.classList.add("before");
-      nextContent.classList.remove("before");
-      nextContent.classList.add("active");
+//       currentContent.classList.remove("active");
+//       currentContent.classList.add("before");
+//       nextContent.classList.remove("before");
+//       nextContent.classList.add("active");
 
 
-      if(index==1){
-        contentItems.forEach((div) => {
-            div.classList.remove('before');
-        });
-      }
+//       if(index==1){
+//         contentItems.forEach((div) => {
+//             div.classList.remove('before');
+//         });
+//       }
 
-      console.log(index)
+//       console.log(index)
   
-      index = nextIndex;
-    }, 10000); 
-  });
+//       index = nextIndex;
+//     }, 50000); 
+//   });
   
